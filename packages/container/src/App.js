e4ed71a610de7ec2f78397fdc4a5b034b83f1421
yashar habibi <habibi4webdesign@gmail.com>
2022-07-16 14:45:25 +0200

    css uniq class generator

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import { BrowserRouter } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+
+const generateClassName = createGenerateClassName({
+  producitonPrefix: "container",
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <h1>Hi there!!!</h1>
+          <hr />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
