7caaab8bcbf6bfdb0840373a342e6692b38808a8
yashar habibi <habibi4webdesign@gmail.com>
2022-07-16 15:04:39 +0200

    fix issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,6 +5,7 @@ import {
 } from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 const generateClassName = createGenerateClassName({
   producitonPrefix: "container",
